1d7141dc830f8843ff0050486948ab0099815e81
vitiminA <kikipham1008@gmail.com>
2023-09-01 14:11:31 +0700

    hello

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// const root = document.getElementById('root')
+
+// var isLight = true
+
+// function blink(){
+//     if (isLight) {
+//         root.style.color = 'red'
+//     } else {
+//         root.style.color ='blue'
+//     }   
+//     isLight = !isLight
+// }
+
+// const blinkblink = setInterval(blink,500)
+var img = document.getElementById("img")
+console.log(img.display)
